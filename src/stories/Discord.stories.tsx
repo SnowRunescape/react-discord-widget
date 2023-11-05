35f7e3e00d5113af15175d613cce3536bef40186
Bruno Caitano <snowrunescape@snowdev.com.br>
2023-11-05 08:26:13 -0400

    feat: implements storybook

---
+++ src/stories/Discord.stories.tsx
@@ -0,0 +1,11 @@
+import Discord from "@Discord/components/Discord";
+
+export default {
+  title: "Discord",
+};
+
+export const ProfileBasic = () => (
+  <div style={{ width: 420 }}>
+    <Discord id="451861943364616192" />
+  </div>
+);
